8e0816eca6dc4ec33c94c9e12d605884cf070b11
K.H.Chathura jayasanka <chathurajayasnka30@gmail.com>
2026-03-28 10:20:00 +0530

    Add Contact link to navbar navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ const navLinks = [
   { name: 'Skills',     href: '#skills' },
   { name: 'Experience', href: '#experience' },
   { name: 'Projects',   href: '#projects' },
+  { name: 'Contact',    href: '#contact' },
 ]
 
 export default function Navbar() {
